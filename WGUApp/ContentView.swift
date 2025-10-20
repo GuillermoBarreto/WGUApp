3ef1d99bc564ca072744da07111eab523ab4304f
Guillermo Barreto <Guilleelias25@gmail.com>
2025-10-19 21:07:52 -0500

    Initial Commit

---
+++ WGUApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WGUApp
+//
+//  Created by Guillermo Barreto on 10/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WGUApp/WGUAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WGUAppApp.swift
+//  WGUApp
+//
+//  Created by Guillermo Barreto on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WGUAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
